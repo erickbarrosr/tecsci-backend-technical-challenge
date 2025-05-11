db47fd605311a2c1fc529ce8e614d1df9b98de3c
Erick Rezende <erickbarrosrezende@gmail.com>
2025-05-11 15:39:01 -0300

    feat: Configurar o roteamento do plantRoutes no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import plantRoutes from "../src/routes/plant.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/plants", plantRoutes);
 
 export { app };
