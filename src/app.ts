adf1e666eabfc9271021adac665a8e402e371268
Erick Rezende <erickbarrosrezende@gmail.com>
2025-05-12 17:02:41 -0300

    feat: Importar o temperatureRoutes no app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import plantRoutes from "../src/routes/plant.routes";
 import inverterRoutes from "../src/routes/inverter.routes";
 import generationRoutes from "../src/routes/generation.routes";
 import powerRoutes from "../src/routes/power.routes";
+import temperatureRoutes from "../src/routes/temperature.routes";
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use("/api/plants", plantRoutes);
 app.use("/api/inverters", inverterRoutes);
 app.use("/api/generations", generationRoutes);
 app.use("/api/powers", powerRoutes);
+app.use("/api/temperatures", temperatureRoutes);
 
 export { app };
