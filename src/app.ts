0bbed26d8292513d1f6bdc66bae58600fa560aa2
Erick Rezende <erickbarrosrezende@gmail.com>
2025-05-12 10:56:54 -0300

    feat: Importar e configurar o inverterRoutes no app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
 import plantRoutes from "../src/routes/plant.routes";
+import inverterRoutes from "../src/routes/inverter.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/plants", plantRoutes);
+app.use("/api/inverters", inverterRoutes);
 
 export { app };
